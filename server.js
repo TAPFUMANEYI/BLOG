a83a4e8dfeab5906ba49e2c575052c10eeac09a3
Kimberly Tapfumaneyi <kimberlytapfumaneyi16@gmail.com>
2024-07-24 15:51:28 +0200

    Posts

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const postsFilePath = path.join(__dirname, 'data', 'posts.json');
+
+// Helper function to read posts from file
+const readPosts = () => {
+    const postsData = fs.readFileSync(postsFilePath);
+    return JSON.parse(postsData);
+};
