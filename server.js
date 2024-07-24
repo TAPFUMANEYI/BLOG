6c6e1716b3a1ab8dc40f632f30c9f3e07f7444ec
Kimberly Tapfumaneyi <kimberlytapfumaneyi16@gmail.com>
2024-07-24 15:40:09 +0200

    logging

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const fs = require('fs');
 const path = require('path');
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the  KIM Blog Platform!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
